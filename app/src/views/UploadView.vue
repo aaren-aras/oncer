bd47b04dcea1f1bee6529e2f05e2590a7d04f351
Aaren Arasaratnam <aaren.aras@gmail.com>
2025-04-27 13:18:43 -0400

    Display upload store images in UI + thumbnail row + reset button

--- app/src/views/UploadView.vue
+++ app/src/views/UploadView.vue
@@ -1,71 +1,136 @@
 <template>
-  <div>
-    <form id="upload-form" @submit.prevent>
-      <input type="file" id="image" @change="uploadFile" accept="image/*" required/>
-    </form>
-  </div>
-  <div v-show="message">{{ message }}</div>
-  <div v-show="result">{{ result }}</div>
+  <section id="upload-view">
+    <div class="selected-view">
+      <img v-if="selectedImage" :src="selectedImage.image" />
+      <p>{{ selectedImage?.prediction }}</p>
+    </div>
+
+    <div class="thumbnail-row">
+      <div
+        v-for="upload in otherImages"
+        :key="upload.image"
+        class="thumbnail"
+        @click="selectImage(upload)"
+      >
+        <img :src="upload.image" alt=""/>
+      </div>
+    </div>
+    <div class="reset-wrapper">
+      <button class="reset" @click="store.resetUploads()">RESET</button>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue';
+  import { ref, computed, watch } from 'vue';
+  import { useUploadStore } from '@/stores/upload';
+  import type { UploadResult } from '@/types';
+  
+  const store = useUploadStore();
+  const selectedImage = ref<UploadResult | null>(null);
 
-  const message = ref(''); // make reactive
-  const result = ref('');
+  const otherImages = computed(() => {
+    return store.uploads.filter((upload) => upload !== selectedImage.value);
+  })
 
-  const uploadFile = async (e: Event) => {
-    const input = e.target as HTMLInputElement;
-    const file = input.files?.[0];
-    if (!file) {
-      message.value = 'No file selected.';
-      return;
-    }
+  watch(
+    () => store.uploads.length, // run callback when number of stored uploads changes
+    () => {
+      selectedImage.value = store.uploads[0] ?? null;
+    },
+    { immediate: true } // initialize selectedImage immediately after refreshes (on mount)
+  );
 
-    const formData = new FormData(); // send file as FormData object
-    formData.append('image', file);
+  const selectImage = (upload: UploadResult) => {
+    selectedImage.value = upload;
+  }
 
-    try {
-      const response = await fetch('http://localhost:5000/predict', {
-        method: 'POST',
-        body: formData,
-      });
+  // const uploads = storeToRefs(store).uploads;
+  // import UploadButton from '@/components/UploadButton.vue';  
+  // const uploadedImage = ref<string | null>(null);
+  // const predictionResult = ref<string>('');
+  // const handleImageUpload = (data: { image: string; prediction: string; originalName: string }) => {
+  //   uploadedImage.value = data.image;
+  //   predictionResult.value = `Tumor ${data.prediction.toLowerCase()}`; 
+  // };
 
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      message.value = 'Upload successful!'; // Update message on success
-      result.value = data.prediction; // Set to prediction result    
-    } catch (e) {
-      console.error('Error uploading file:', e);
-      message.value = 'File upload failed.';
-      result.value = ''; // Clear result on error
-    }
-  };
 </script>
 
 <style scoped lang="scss">
-  template {
-    
-  }
+  @use '../../assets/scss/global.scss' as *;
+  // @use '../../assets/scss/palette';
 
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
+  #upload-view {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
+    width: 100%;
+
+    .selected-view {
       display: flex;
+      flex-direction: column;
       align-items: center;
+      gap: 1em;
+      // border: 1px solid red;
+
+      img {
+        // width: 50rem;
+        // border: 1px solid var(--color-border);
+        border: 2px ridge $accent-3;
+        border-radius: 5px;
+        // color: palette.$accent-3;
+
+      }
+
+      p {
+        font-size: 1.25rem;
+        font-weight: bold;
+      }
     }
-  }
 
-  #result {
-    font-weight: bold;
-    margin-top: 20px;
-    color: green;
-  }
+    .thumbnail-row {
+      display: flex;
+      justify-content: flex-start;
+      overflow-x: auto;
+      gap: 1rem;
+      padding: 10px;
+      border-top: 1px solid var(--color-border);
 
-  #message {
-    color: red;
+      .thumbnail {
+        flex: 0 0 auto;
+        width: 10rem;
+        height: 10rem;
+        margin-top: 1rem;
+        cursor: pointer;
+
+        img {
+          object-fit: cover;
+          border-radius: 20px;
+          @include transition-ease;
+
+          &:hover {
+            transform: scale(1.05);
+          }
+        }
+      }
+    }
+
+    .reset-wrapper {
+      .reset {
+        // background-color: var(--);
+        background-color: $accent-3;
+        color: #fff;
+        border: 1px solid var(--color-border);
+        padding: 0.5rem 1rem;
+        border-radius: 8px;
+        cursor: pointer;  
+        @include transition-ease;
+        
+        &:hover {
+          opacity: 0.6;
+        }
+      }
+    }
   }
 </style>
