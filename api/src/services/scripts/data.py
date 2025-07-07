3c8a29e6b0d1ad097eb259cb801d730021f23a84
Aaren Arasaratnam <aaren.aras@gmail.com>
2025-07-07 00:13:22 -0400

    Reorganize func order, add func type hints and return types

--- api/src/services/scripts/data.py
+++ api/src/services/scripts/data.py
@@ -1,4 +1,4 @@
-import pathlib
+from pathlib import Path
 import json
 
 from tqdm import tqdm
@@ -25,7 +25,7 @@ for split in ['train', 'valid', 'test']:
     (METADATA_DIR / split).mkdir(parents=True, exist_ok=True)
 
 
-def normalize_modality(img):
+def normalize_modality(img: np.ndarray) -> np.ndarray:
     '''Normalize MRI image slice to 8-bit greyscale to standardize pixel intensities and highlight structure over brightness.'''
     img = np.nan_to_num(img) # handle any corrupted/missing values
     img = np.clip(img, 0, np.percentile(img, 99)) # cut off extremes (99th %tile)
@@ -33,7 +33,34 @@ def normalize_modality(img):
     return (img * 255).astype(np.uint8) # scale to [0, 255] (2e8)
 
 
-def save_slice(img_stack, mask_slice, metadata, subject_id, slice_idx, split):
+def process_subject(subject_path: Path) -> list[tuple[np.ndarray, np.ndarray, str, int]]:
+    '''Given a BraTS subject (BraTS_2021_0xxxx):
+      - Stack all 4 MRI modalities (T1, T1CE, T2, FLAIR) into a 4-channel 3D image volume. 
+      - Extract MRI image slices from both the image volume and corresponding 3D segmentation mask.
+    '''
+    subject_id = subject_path.name
+    
+    imgs = []
+    for m in MODALITIES:
+        m_nii = nib.load(subject_path / f'{subject_id}_{m}.nii.gz')
+        data = normalize_modality(m_nii.get_fdata()) # Nifti1Image obj -> np arr (raw voxels as float) -> np arr (raw voxels normalized to [0, 255])
+        imgs.append(data)
+    stacked = np.stack(imgs, axis=-1) # shape (H, W, D) x 4 -> (H, W, D, 4) (append new dim at the end)
+
+    seg_nii = nib.load(subject_path / f'{subject_id}_seg.nii.gz')
+    mask = seg_nii.get_fdata().astype(np.uint8) # Nifti1Image obj -> np arr (raw voxels as float) -> np arr (raw voxels as uint8)  
+    mask[mask == 4] = 3 # remap label 4 -> 3 for convenience
+
+    slices = []
+    for i in range(stacked.shape[2]): # per axial slice (z-resolution)
+        img_slice = stacked[:, :, i, :] # shape (H, W, 4)
+        mask_slice = mask[:, :, i] # shape (H, W)
+        slices.append((img_slice, mask_slice, subject_id, i))
+
+    return slices
+
+
+def save_slice(img_stack: np.ndarray, mask_slice: np.ndarray, subject_id: str, slice_idx: int, split: str) -> None:
     '''Save MRI image slice, corresponding segmentation mask, and per-slice metadata to their respective subsubdirectories.'''
     img_path = IMG_DIR / split / f'{subject_id}_slice{slice_idx:03d}.npy' # e.g., 5 -> 005
     mask_path = MASK_DIR / split / f'{subject_id}_slice{slice_idx:03d}_mask.npy'
@@ -64,41 +91,14 @@ def save_slice(img_stack, mask_slice, metadata, subject_id, slice_idx, split):
         json.dump(metadata, file, default=str) # ->str if unserializable
 
 
-def process_subject(subject_path):
-    '''Given a BraTS subject (BraTS_2021_0xxxx):
-      - Stack all 4 MRI modalities (T1, T1CE, T2, FLAIR) into a 4-channel 3D image volume. 
-      - Extract MRI image slices from both the image volume and corresponding 3D segmentation mask.
-    '''
-    subject_id = subject_path.name
-    
-    imgs = []
-    for m in MODALITIES:
-        m_nii = nib.load(subject_path / f'{subject_id}_{m}.nii.gz')
-        data = normalize_modality(m_nii.get_fdata()) # Nifti1Image obj -> np arr (raw voxels as float) -> np arr (raw voxels normalized to [0, 255])
-        imgs.append(data)
-    stacked = np.stack(imgs, axis=-1) # shape (H, W, D) x 4 -> (H, W, D, 4) (append new dim at the end)
-
-    seg_nii = nib.load(subject_path / f'{subject_id}_seg.nii.gz')
-    mask = seg_nii.get_fdata().astype(np.uint8) # Nifti1Image obj -> np arr (raw voxels as float) -> np arr (raw voxels as uint8)  
-    mask[mask == 4] = 3 # remap label 4 -> 3 for convenience
-
-    slices = []
-    for i in range(stacked.shape[2]): # per axial slice (z-resolution)
-        img_slice = stacked[:, :, i, :] # shape (H, W, 4)
-        mask_slice = mask[:, :, i] # shape (H, W)
-        slices.append((img_slice, mask_slice, subject_id, i))
-
-    return slices
-
-
-def main():
+def main() -> None:
     all_slices = []
     
     subjects = sorted([p for p in BRATS_DIR.iterdir()])
     for subject_path in tqdm(subjects, desc='Processing BraTS2021 subjects'):
         slices = process_subject(subject_path)
         all_slices.extend(slices)
-    # print(f'Total 2D Slices: {len(all_slices)}')
+    print(f'Total 2D Slices: {len(all_slices)}')
     
     # Split MRI image slices into training, validation, and test sets (70-15-15)
     train, temp = train_test_split(all_slices, test_size=0.3, random_state=2025) # 70% train, 30% temp
@@ -106,8 +106,8 @@ def main():
 
     splits = [(train, 'train'), (valid, 'valid'), (test, 'test')]
     for split_data, split_name in splits:
-        for img, mask, metadata, subject_id, idx in tqdm(split_data, desc=f'Saving \'{split_name}\' slices'):
-            save_slice(img, mask, metadata, subject_id, idx, split_name) 
+        for img, mask, subject_id, idx in tqdm(split_data, desc=f'Saving \'{split_name}\' slices'):
+            save_slice(img, mask, subject_id, idx, split_name) 
 
     print('*COMPLETE: images have been distributed across training, validation, and test sets')
 
