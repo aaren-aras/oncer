fecb3ba583e179e9b120d40af5967367dd27b0a8
Aaren Arasaratnam <aaren.aras@gmail.com>
2025-12-17 16:14:42 -0500

    Add CORS and temp return dict for frontend

--- api/src/services/main.py
+++ api/src/services/main.py
@@ -1,19 +1,22 @@
+from pathlib import Path
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
+import traceback
 
 import tensorflow as tf
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, UploadFile, File, HTTPException
 import numpy as np
 
-from scripts.preprocessing import preprocess_uploads
-from scripts.segmentation import create_overlay, compute_segmentation_stats
+from .scripts.preprocessing import preprocess_uploads
+from .scripts.segmentation import create_overlay, compute_segmentation_stats
 
-MODEL_PATH = 'models/oncer_model.keras'
+MODEL_PATH = Path(__file__).resolve().parent / 'models/oncer_model.keras' # oncer/api/src/services/models
 model: tf.keras.Model | None = None
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None, None]:
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     """
     Load model once at startup.
     """
@@ -27,7 +30,14 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None, None]:
        print('Shutting down microservice...')
         
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173'], 
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.post('/predict')
@@ -49,18 +59,20 @@ async def predictTumors(
 
         # Run inference and send response 
         seg_logits = model.predict(input_tensor)
-        seg_mask = np.argmax(seg_mask.squeeze(), axis=-1) # shape (H, W)
         seg_mask = np.argmax(seg_logits[0], axis=-1) # shape (1, H, W, 4) -> (H, W, 4) -> (H, W), most likely class per pixel
-        tumor_present = bool(np.any(seg_mask > 0))
+        # tumor_present = bool(np.any(seg_mask > 0))
+        prediction = 'Tumour detected my boy!!! Sucks for you' if bool(np.any(seg_mask > 0)) else 'No tumour bro dw'
 
         overlay = create_overlay(seg_mask)
         stats = compute_segmentation_stats(seg_mask)
 
         return {
-            'tumor_present': tumor_present,
+            # 'tumor_present': tumor_present,
+            'prediction': prediction,
             'overlay': overlay,
             'stats': stats, 
         }
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f'Error during inference: {str(e)}')
+    except Exception:
+        traceback.print_exc()
+        raise 
     
